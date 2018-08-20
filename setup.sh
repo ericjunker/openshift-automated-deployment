8ac05e4685ae65ac24ebba43385c9d9e2abb04ad
Eric Junker <ejunker@redhat.com>
2018-08-20 13:47:39 -0400

    templates for tasks

--- setup.sh
+++ setup.sh
@@ -152,13 +152,12 @@ oc set triggers dc openshift-tasks --manual
 oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n tasks
 #next step: configure Jenkins over CLI
 #pull down Jenkins CLI jar file from jenkins, which requires Java
-yum install -y java
-oc project cicd
-#jenkinsURL= $(oc get route jenkins --template='{{ .spec.host }}')
-wget http://jenkins-cicd.apps.$GUID.example.opentlc.com/jnlpJars/jenkins-cli.jar --no-check-certificate
-java -jar jenkins-cli.jar -s http://jenkins-cicd.apps.$GUID.example.opentlc.com -uauth admin:password help
+# yum install -y java
+# oc project cicd
+# wget http://jenkins-cicd.apps.$GUID.example.opentlc.com/jnlpJars/jenkins-cli.jar --no-check-certificate
+# java -jar jenkins-cli.jar -s http://jenkins-cicd.apps.$GUID.example.opentlc.com -uauth admin:password help
 
-
+#doing Jenkins configuration sucks, let's try an alternate approach
 
 
 } 2>&1 | tee logfile
